b1c91d5335920a658ada7422bf69f9c77ab44530
tinatrinh1012 <68746212+tinatrinh1012@users.noreply.github.com>
2023-04-08 22:22:00 -0500

    create checkout form for user input

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartService } from '../cart.service';
 
 @Component({
@@ -9,7 +10,21 @@ import { CartService } from '../cart.service';
 export class CartComponent {
   // PROPERTY
   items = this.cartService.getItems();
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
 
   // CONSTRUCTOR
-  constructor(private cartService: CartService) {}
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  // METHODS
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
